9ea1e3b6023d1e4a3f8def469bd6d5311418a8a8
René Eber <eber.rene@gmail.com>
2021-12-12 14:06:11 +0100

    add run code functionality

---
+++ toggle_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from IPython.core.display import display, HTML
+
+run_code_str = '''
+<form action="javascript:Jupyter.notebook.execute_cell()"><input type ="submit" id="runButton" value="Run Cell"></form>
+'''
+
+display(HTML(run_code_str))
+
+def run_code():
+    display(HTML(run_code_str))
+
